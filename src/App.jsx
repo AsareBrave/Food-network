2e9f413b3deb023a0e4f106e4b487371b6134938
quadri090 <99067374+quadri090@users.noreply.github.com>
2023-08-25 22:17:51 +0100

    feat: added website header/navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
+import Header from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-red-700 font-bold text-5xl font-Poppins'>
-        <h1>This is the testing of the font</h1>
-       </div>
-
-       <h1 className='text-5xl '>This is the testing of the font</h1>
+     <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Header() {
+
+  return (
+    <div className='h-[32px] sm:h-[40px] mx-[20px] my-[22px]  lg:my-[42px] flex justify-between items-center text-[10px] sm:text-[16px] font-normal font-Poppins leading-normal'>
+        <div className='w-[72px] sm:w-[90px] h-[24px] sm:h-[30px] text-[16px] sm:text-[20px] font-medium flex items-center justify-center'>
+            <p>Food<span className='text-primary-red'>Bag</span></p>
+        </div>
+        <ul className='flex items-center justify-center gap-[20px] sm:gap-[30px] lg:gap-[50px]'>
+            <li className='text-primary-grey'>
+                <a href="#cart">Cart</a>
+
+            </li>
+            <li className='text-primary-grey'>
+                <a href="#login">Login</a>
+
+            </li>
+            <li className='w-[74px] sm:w-[92px] h-[32px] sm:h-[40px] flex items-center justify-center rounded-[5px] text-primary-red font-medium border border-primary-red'>
+                <a href="#signup">Sign Up</a>
+
+            </li>
+        </ul>
+    </div>
+  )
+}
